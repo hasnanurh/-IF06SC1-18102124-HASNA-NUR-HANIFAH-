85b8322dce21220a9e38f2676d5b98e5144d6c00
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2020-11-16 10:16:41 +0700

    create my data

--- Praktikum6/app/src/main/java/com/hasnanurhanifah_18102124/praktikum6/MyData.kt
+++ Praktikum6/app/src/main/java/com/hasnanurhanifah_18102124/praktikum6/MyData.kt
@@ -1,4 +1,13 @@
 package com.hasnanurhanifah_18102124.praktikum6
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
 class MyData {
+    @Parcelize
+    data class MyData(
+        var name: String,
+        var description: String,
+        var photo: String
+    ) : Parcelable
 }
