fce24e3d3e489324c531a4bb58d0c0b9d9029053
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2020-12-14 08:39:54 +0700

    create button permission

--- Praktikum8/app/src/main/java/com/hasnanurhanifah_18102124/praktikum8/MainActivity.kt
+++ Praktikum8/app/src/main/java/com/hasnanurhanifah_18102124/praktikum8/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.hasnanurhanifah_18102124.praktikum8
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val SMS_REQUEST_CODE = 101
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_permission.setOnClickListener {
+            if(ActivityCompat.checkSelfPermission(this, Manifest.permission.RECEIVE_SMS) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.RECEIVE_SMS), SMS_REQUEST_CODE)
+            }
+        }
     }
 }
