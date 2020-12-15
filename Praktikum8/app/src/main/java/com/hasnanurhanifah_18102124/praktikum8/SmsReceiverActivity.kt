ca08efdf916c9efbd04abc3dbde3459fc3783f56
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2020-12-15 19:16:20 +0700

    finished step tambahan deteksi pesan penipuan

--- Praktikum8/app/src/main/java/com/hasnanurhanifah_18102124/praktikum8/SmsReceiverActivity.kt
+++ Praktikum8/app/src/main/java/com/hasnanurhanifah_18102124/praktikum8/SmsReceiverActivity.kt
@@ -1,14 +1,19 @@
 package com.hasnanurhanifah_18102124.praktikum8
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_sms_receiver.*
 
+@Suppress("TYPE_INFERENCE_ONLY_INPUT_TYPES_WARNING")
 class SmsReceiverActivity : AppCompatActivity() {
     companion object {
         const val EXTRA_SMS_NO = "extra_sms_no"
         const val EXTRA_SMS_MESSAGE = "extra_sms_message"
     }
+
+    @SuppressLint("SetTextI18n")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +21,19 @@ class SmsReceiverActivity : AppCompatActivity() {
 
         title = getString(R.string.incoming_message)
         val senderNo = intent.getStringExtra(EXTRA_SMS_NO)
-        val senderMessage = intent.getStringExtra(EXTRA_SMS_MESSAGE)
-        tv_from.text = getString(R.string.coming_from) + ": " + senderNo
-        tv_message.text = senderMessage
+        val senderMessage = intent.getStringExtra(EXTRA_SMS_MESSAGE)?.toLowerCase()
+        val penipuan = arrayOf("hadiah", "blogspot", "wordpress", "pulsa",
+        "selamat", "transfer", "mobil", "polisi", "rumah")
+        val match = penipuan.filter { it in senderMessage!!.split(" ") }
+        if (match.size >= 2){
+            tv_from.text = getString(R.string.coming_from) + ": " + senderNo
+            tv_message.text = "WARNING!! PESAN PENIPUAN\n\n" + senderMessage
+            tv_message.setTextColor(ContextCompat.getColor(this, android.R.color.holo_red_light))
+        } else {
+            tv_from.text = getString(R.string.coming_from) + ": " + senderNo
+            tv_message.text = senderMessage
+        }
         btn_close.setOnClickListener { finish() }
+
     }
 }
