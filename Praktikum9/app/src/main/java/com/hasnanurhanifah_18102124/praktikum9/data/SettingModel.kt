a7d9b3f7666588b6abf7dc32fcc5586f0b87e359
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2020-12-25 21:06:51 +0700

    create class setting model

---
+++ Praktikum9/app/src/main/java/com/hasnanurhanifah_18102124/praktikum9/data/SettingModel.kt
@@ -0,0 +1,15 @@
+package com.hasnanurhanifah_18102124.praktikum9.data
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+class SettingModel {
+    @Parcelize
+    data class SettingModel (
+        var name: String? = null,
+        var email: String? = null,
+        var age: Int = 0,
+        var phoneNumber: String? = null,
+        var isDarkTheme: Boolean = false
+    ): Parcelable
+}
