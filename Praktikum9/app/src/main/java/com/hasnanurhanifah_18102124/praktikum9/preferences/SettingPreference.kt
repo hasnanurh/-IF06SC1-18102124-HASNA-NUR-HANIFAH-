630e5c9b13096e7d69db8d348553115a0665a9a0
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2020-12-25 21:16:36 +0700

    Add class Setting Preference

---
+++ Praktikum9/app/src/main/java/com/hasnanurhanifah_18102124/praktikum9/preferences/SettingPreference.kt
@@ -0,0 +1,29 @@
+package com.hasnanurhanifah_18102124.praktikum9.preferences
+
+import android.content.Context
+import com.hasnanurhanifah_18102124.praktikum9.data.SettingModel
+
+class SettingPreference {
+    internal class SettingPreference(context: Context) {
+        companion object {
+            private const val PREFS_NAME = "setting_pref"
+            private const val NAME = "name"
+            private const val EMAIL = "email"
+            private const val AGE = "age"
+            private const val PHONE_NUMBER = "phone"
+            private const val THEME = "theme"
+        }
+
+        private val preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        fun setSetting(value: SettingModel) {
+            val editor = preferences.edit()
+            editor.putString(NAME, value.name)
+            editor.putString(EMAIL, value.email)
+            editor.putInt(AGE, value.age)
+            editor.putString(PHONE_NUMBER, value.phoneNumber)
+            editor.putBoolean(THEME, value.isDarkTheme)
+            editor.apply()
+        }
+
+    }
+}
