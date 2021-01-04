effeb2047fcf4f56ed91da219b49e937ef81f09d
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2021-01-04 19:08:38 +0700

    create class DatabaseContract

---
+++ Praktikum10/app/src/main/java/com/hasnanurhanifah_18102124/praktikum10/db/DatabaseContract.kt
@@ -0,0 +1,16 @@
+package com.hasnanurhanifah_18102124.praktikum10.db
+
+import android.provider.BaseColumns
+
+internal class DatabaseContract {
+    internal class QuoteColumns : BaseColumns {
+        companion object {
+            const val TABLE_QUOTE = "quote"
+            const val _ID = "_id"
+            const val TITLE = "title"
+            const val DESCRIPTION = "description"
+            const val CATEGORY = "category"
+            const val DATE = "date"
+        }
+    }
+}
