bbf40fc785053aefcb0e245152c1e36e038ec360
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2021-01-15 20:51:48 +0700

    create code for validate user

--- Praktikum11/app/src/main/java/com/hasnanurhanifah_18102124/praktikum11/MainActivity.kt
+++ Praktikum11/app/src/main/java/com/hasnanurhanifah_18102124/praktikum11/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.hasnanurhanifah_18102124.praktikum11
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.hasnanurhanifah_18102124.praktikum11.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            val intent = Intent(this@MainActivity, SigninActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            val intent = Intent(this@MainActivity, SigninActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
 }
