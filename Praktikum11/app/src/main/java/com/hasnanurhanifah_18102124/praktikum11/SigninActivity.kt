409c338e2586ca99ab1e90edc6aa70b6556cb558
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2021-01-15 20:06:29 +0700

    add binding & onclick for component on sign in activity

--- Praktikum11/app/src/main/java/com/hasnanurhanifah_18102124/praktikum11/SigninActivity.kt
+++ Praktikum11/app/src/main/java/com/hasnanurhanifah_18102124/praktikum11/SigninActivity.kt
@@ -2,10 +2,28 @@ package com.hasnanurhanifah_18102124.praktikum11
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.hasnanurhanifah_18102124.praktikum11.databinding.ActivitySigninBinding
 
-class SigninActivity : AppCompatActivity() {
+class SigninActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySigninBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signin)
+        binding = ActivitySigninBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnSign.setOnClickListener(this)
+        binding.tvSignUp.setOnClickListener(this)
+        binding.btnEmail.setOnClickListener(this)
+        binding.btnPhone.setOnClickListener(this)
+        auth = Firebase.auth
     }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
 }
