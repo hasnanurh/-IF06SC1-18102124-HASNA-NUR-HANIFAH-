012263a997187cb26b76b18ef435f500f1de4837
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2021-01-15 20:56:51 +0700

    add onclick & function email verification and signOut

--- Praktikum11/app/src/main/java/com/hasnanurhanifah_18102124/praktikum11/MainActivity.kt
+++ Praktikum11/app/src/main/java/com/hasnanurhanifah_18102124/praktikum11/MainActivity.kt
@@ -3,12 +3,14 @@ package com.hasnanurhanifah_18102124.praktikum11
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import com.hasnanurhanifah_18102124.praktikum11.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var auth: FirebaseAuth
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,6 +18,9 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         auth = Firebase.auth
+        binding.btnSignOut.setOnClickListener(this)
+        binding.btnEmailVerify.setOnClickListener(this)
+
         val currentUser = auth.currentUser
         if (currentUser == null) {
             val intent = Intent(this@MainActivity, SigninActivity::class.java)
@@ -34,5 +39,44 @@ class MainActivity : AppCompatActivity() {
             finish()
         }
     }
+
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btnSignOut -> {
+                signOut()
+            }
+            R.id.btnEmailVerify -> {
+                sendEmailVerification()
+            }
+        }
+    }
+
+    private fun sendEmailVerification() {
+        binding.btnEmailVerify.isEnabled = false
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                binding.btnEmailVerify.isEnabled = true
+                if (task.isSuccessful) {
+                    Toast.makeText(baseContext,
+                        "Verification email sent to ${user.email} ",
+                        Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(baseContext,
+                        "Failed to send verification email.",
+                        Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
+    private fun signOut() {
+        auth.signOut()
+        val currentUser = auth.currentUser
+        if (currentUser == null) {
+            val intent = Intent(this@MainActivity, SigninActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
 
 }
