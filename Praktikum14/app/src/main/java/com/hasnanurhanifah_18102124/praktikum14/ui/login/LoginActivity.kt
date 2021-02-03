5f713422c05396cc2c6534e1af42bd59becc47e7
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2021-02-03 11:07:01 +0700

    Create Login Activity

---
+++ Praktikum14/app/src/main/java/com/hasnanurhanifah_18102124/praktikum14/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.hasnanurhanifah_18102124.praktikum14.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hasnanurhanifah_18102124.praktikum14.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
