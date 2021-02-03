894039f00ced1af272b61b70f852afcc6aa6fe72
Hasna Nur Hanifah <hasnanurhanifah24@gmail.com>
2021-02-03 12:05:02 +0700

    Add function getMyqoutes, getClassQuotes, getAllQuotes, addQuote, updateQuote, deleteQuote and login

--- Praktikum14/app/src/main/java/com/hasnanurhanifah_18102124/praktikum14/api/MainPresenter.kt
+++ Praktikum14/app/src/main/java/com/hasnanurhanifah_18102124/praktikum14/api/MainPresenter.kt
@@ -2,6 +2,8 @@ package com.hasnanurhanifah_18102124.praktikum14.api
 
 import com.hasnanurhanifah_18102124.praktikum14.CoroutineContextProvider
 import com.hasnanurhanifah_18102124.praktikum14.`interface`.MainView
+import com.hasnanurhanifah_18102124.praktikum14.model.Login
+import com.hasnanurhanifah_18102124.praktikum14.model.Message
 import com.hasnanurhanifah_18102124.praktikum14.model.QuoteResponse
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -35,6 +37,136 @@ class MainPresenter(
                     }
                 })
             } catch (e: Exception) {
+            }
+        }
+    }
+
+    fun getClassQuotes(token:String?) {
+        GlobalScope.launch (context.main){
+            try {
+                ApiMain().services.getClassQuotes("Bearer "+token).enqueue(object :
+                    Callback<QuoteResponse> {
+                    override fun onResponse(call: Call<QuoteResponse>, response: Response<QuoteResponse>) {
+                        if(response.code() == 200) {
+                            response.body()?.quotes?.let {
+                                view.resultQuote(it)
+                            }
+                        }
+                    }
+                    override fun onFailure(call: Call<QuoteResponse>, t: Throwable) {
+                        view.showMessage("Koneksi Terputus")
+                    }
+                })
+            }
+            catch (e:Exception){
+            }
+        }
+    }
+    fun getAllQuotes(token: String) {
+        GlobalScope.launch (context.main){
+            try {
+                ApiMain().services.getAllQuotes("Bearer "+token).enqueue(object :
+                    Callback<QuoteResponse> {
+                    override fun onResponse(call: Call<QuoteResponse>, response: Response<QuoteResponse>) {
+                        if(response.code() == 200) {
+                            response.body()?.quotes?.let {
+                                view.resultQuote(it)
+                            }
+                        }
+                    }
+                    override fun onFailure(call: Call<QuoteResponse>, t: Throwable) {
+                        view.showMessage("Koneksi Terputus")
+                    }
+                })
+            }
+            catch (e:Exception){
+            }
+        }
+    }
+    fun addQuote(token:String, title:String, description: String) {
+        GlobalScope.launch (context.main){
+            try {
+                ApiMain().services.addQuote("Bearer "+token,title,description).enqueue(object :
+                    Callback<Message> {
+                    override fun onResponse(call: Call<Message>, response: Response<Message>) {
+                        if(response.code() == 200) {
+                            response.body()?.message?.let {
+                                view.showMessage(it)
+                            }
+                        }
+                    }
+                    override fun onFailure(call: Call<Message>, t: Throwable) {
+                        view.showMessage("Koneksi Terputus")
+                    }
+                })
+            }
+            catch (e:Exception){
+            }
+        }
+    }
+    fun updateQuote(token:String, quote_id:String, title:String, description: String) {
+        GlobalScope.launch (context.main){
+            try {
+                ApiMain().services.updateQuote("Bearer "+token,quote_id,title,description).enqueue(object :
+                    Callback<Message> {
+                    override fun onResponse(call: Call<Message>, response: Response<Message>) {
+                        if(response.code() == 200) {
+                            response.body()?.message?.let {
+                                view.showMessage(it)
+                            }
+                        }
+                    }
+                    override fun onFailure(call: Call<Message>, t: Throwable) {
+                        view.showMessage("Koneksi Terputus")
+                    }
+                })
+            }
+            catch (e:Exception){
+            }
+        }
+    }
+    fun deleteQuote(token: String, quote_id: String) {
+        GlobalScope.launch (context.main){
+            try {
+                ApiMain().services.deleteQuote("Bearer "+token,quote_id).enqueue(object :
+                    Callback<Message> {
+                    override fun onResponse(call: Call<Message>, response: Response<Message>) {
+                        if(response.code() == 200) {
+                            response.body()?.message?.let {
+                                view.showMessage(it)
+                            }
+                        }
+                    }
+                    override fun onFailure(call: Call<Message>, t: Throwable) {
+                        view.showMessage("Koneksi Terputus")
+                    }
+                })
+            }
+            catch (e:Exception){
+            }
+        }
+    }
+    fun login(nim:String, password:String) {
+        GlobalScope.launch (context.main){
+            try {
+                ApiMain().services.login(nim,password).enqueue(object :
+                    Callback<Login> {
+                    override fun onResponse(call: Call<Login>, response: Response<Login>) {
+                        if(response.code()==200){
+                            response.body()?.let { it1 -> view.resultLogin(it1) }
+                            view.showMessage("Login Berhasil")
+                        }
+                        else{
+                            view.showMessage("Login Gagal")
+                        }
+
+                    }
+                    override fun onFailure(call: Call<Login>, t: Throwable) {
+                        view.showMessage("Koneksi Terputus")
+                    }
+                })
+            }
+            catch (e:Exception){
             }
         }
     }
